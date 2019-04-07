e63dc6bb2c6e8d6f669c5b778235fccd09fd6fa0
lertoss <joniclife@gmail.com>
2019-04-07 12:35:13 +0700

    Add run file

---
+++ run.sh
@@ -0,0 +1,8 @@
+# bin/bash
+
+### This file is used to run the project. ###
+### Manual project launch is not recommended. ###
+### Running a project without the required ###
+### parameters will cause an error. ###
+
+export LD_LIBRARY_PATH=lib/ && ./bin/main
