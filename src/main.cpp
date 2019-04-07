a19a68e6538d38f990ef8cb39d219f2c9af8936f
lertoss <joniclife@gmail.com>
2019-04-07 12:18:26 +0700

    Add test SFML code to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <cstdlib>
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    RenderWindow window(VideoMode(600, 600), "Hello, world!", Style::Close);
+
+    CircleShape circle(300);
+    circle.setFillColor(Color::Blue);
+
+    while (window.isOpen())
+    {
+        Event e;
+        while(window.pollEvent(e))
+        {
+            if (e.type == Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+
     return 0;
 }
