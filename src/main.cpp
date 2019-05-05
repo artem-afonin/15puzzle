330eabb1de17569062d350fad5aaa82827fadef8
Victoria <vika.derevcova@mail.ru>
2019-05-05 14:14:59 +0700

    Reduce screen refresh time

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ using namespace sf;
 int main()
 {
     RenderWindow window(VideoMode(600, 600), "Hello, world!", Style::Close);
-    window.setFramerateLimit(20);
+    window.setFramerateLimit(6);
 
     Mainmenu* mainmenu;
     Settings* settingsmenu;
