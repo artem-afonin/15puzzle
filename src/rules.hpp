54954f93f0089a5d59d938ff118117d47e847127
lertoss <joniclife@gmail.com>
2019-05-09 12:38:54 +0700

    Refactor rules

--- src/rules.hpp
+++ src/rules.hpp
@@ -12,16 +12,7 @@ private:
 
     Text exitButton;
 
-    Text gameRules1;
-    Text gameRules2;
-    Text gameRules3;
-    Text gameRules4;
-    Text gameRules5;
-    Text gameRules6;
-    Text gameRules7;
-    Text gameRules8;
-    Text gameRules9;
-    Text gameRules10;
+    Text gameRules[10];
 
     void colorExitButton(RenderWindow &window);
 
