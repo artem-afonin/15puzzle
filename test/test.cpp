5349bed9f9edd620a69bae1356d5da982e65e946
lertoss <joniclife@gmail.com>
2019-05-23 19:31:59 +0700

    Delete junk comments

--- test/test.cpp
+++ test/test.cpp
@@ -7,23 +7,6 @@ using namespace sf;
 
 bool debug = true;
 
-/*
-TEST(PrimaryTest, FirstPrimaryTest)
-{
-    EXPECT_EQ (18.0, 18.0);
-}
-
-TEST(PrimaryTest, SecondPrimaryTest)
-{
-    EXPECT_EQ (25.4, 25.4);
-}
-
-TEST(PrimaryTest, ThirdPrimaryTest)
-{
-    EXPECT_EQ (50.3321, 50.3321);
-}
-*/
-
 TEST(Textbox, availableCharactersFile)
 {
 	Textbox box;
